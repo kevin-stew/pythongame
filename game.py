59797fe4e0ca0ab3d18497fbb5744b998efa2446
Thomas Morris <thomasmorris682@gmail.com>
2022-01-14 07:52:25 -0900

    first functions

--- game.py
+++ game.py
@@ -1,2 +1,20 @@
+import random
+
+options = ['rock','paper', 'scissors']
+
+def comp_op(pick):
+    choice = random.randint(0,2)
+    return pick[choice]
+
+def game():
+    user_choice = input("Please enter a number- 1: Rock 2: Scissors 3: Paper")
+    comp_choice = comp_op()
+    if user_choice == comp_op:
+        "Tie"
+
+
+print(comp_op(options))
+
+
 
 
